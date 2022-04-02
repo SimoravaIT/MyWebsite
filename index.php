0f24235a5a4aeffa072d7f8bcefbbc25f90d8868
Simone Rava <ravasimoneit@gmail.com>
2022-04-02 12:56:05 +0200

    update

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <html>
  <head>
-  <title>Simo' s webpage </title>
+  <title>Simo' s webpage' s title </title>
  </head>
  <body>
- <?php echo '<p>Benvenuti nella mia pagina</p>'; ?> 
+    The -!DOCTYPE html- declaration defines that this document is an HTML5 document
+    The -html- element is the root element of an HTML page
+    The -head- element contains meta information about the HTML page
+    The -title- element specifies a title for the HTML page (which is shown in the browser's title bar or in the page's tab)
+    The -body- element defines the document's body, and is a container for all the visible contents, such as headings, paragraphs, images, hyperlinks, tables, lists, etc.
+    The -h1- element defines a large heading
+    The -p- element defines a paragraph
+
+    <h1> example of an h1 header</h1>
+    <p> example of a p paragraph</p>
+    <?php echo '<p>Benvenuti nella mia pagina</p>'; ?> 
  </body>
 </html>
